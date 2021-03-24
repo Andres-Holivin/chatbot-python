a340ec8d59ef31149e59ba3507acd4aa7c5f78a6
Andres Holivin <andres.holivin@binus.edu>
2021-03-25 01:54:33 +0700

    test

--- nltk_utils.py
+++ nltk_utils.py
@@ -7,19 +7,19 @@ def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 def stem(word):
     return stemmer.stem(word.lower())
-def bag_of_words(tokenized_sentence,all_words):
+def bag_of_words(tokenized_sentence,words):
     """
-
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
-    tokenized_sentence=[stem(w) for w in tokenized_sentence]
-    bag=np.zeros(len(all_words),dtype=np.float32)
-    for idx,w in enumerate(all_words):
-        if w in tokenized_sentence:
+    sentence_words=[stem(w) for w in tokenized_sentence]
+    bag=np.zeros(len(words),dtype=np.float32)
+    for idx,w in enumerate(words):
+        if w in sentence_words:
             bag[idx]=1.0
     return bag
-if __name__=="__main__":
-    sentence=["hello","how","are","you"]
-    words=["hi","hello","I","you","bye","thank","cool"]
-    bag = bag_of_words(sentence,words)
-    print(bag)
     
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset,DataLoader
 
-from Model import NeuralNer
+from Model import NeuralNet
 
 with open("object.json","r")as f:
     intents=json.load(f)
@@ -29,23 +29,22 @@ all_words=[stem(w)for w in all_words if w not in ignore_words]
 all_words=sorted(set(all_words))
 tags = sorted(set(tags))
 print(tags)
-print("tags={tags}")
 
-x_train=[]
+X_train=[]
 y_train=[]
 for (pattern_sentence,tag) in xy:
     bag = bag_of_words(pattern_sentence,all_words)
-    x_train.append(bag)
+    X_train.append(bag)
     label= tags.index(tag)
     y_train.append(label)
 
-x_train=np.array(x_train)
+X_train=np.array(X_train)
 y_train=np.array(y_train)
 
 class ChatDataset(Dataset):
     def __init__(self):
-        self.n_samples=len(x_train)
-        self.x_data=x_train
+        self.n_samples=len(X_train)
+        self.x_data=X_train
         self.y_data=y_train
     def __getitem__(self,index):
         return self.x_data[index],self.y_data[index]
@@ -55,29 +54,28 @@ class ChatDataset(Dataset):
 batch_size=8    
 hidden_size=8
 output_size=len(tags)
-input_size=len(x_train[0])
+input_size=len(X_train[0])
 lerning_rate=0.001
 num_epochs=1000
-
-print(input_size,len(all_words))
-print(output_size,tags)
+print(input_size,output_size)
+# print(input_size,len(all_words))
+# print(output_size,tags)
 
 dataset=ChatDataset()
 train_loader=DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,num_workers=2)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = NeuralNer(input_size,hidden_size,output_size).to(device)
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer =  torch.optim.Adam(model.parameters(),lr=lerning_rate)
-
 for epoch in range(num_epochs):
     for(words,labels) in train_loader:
         words = words.to(device)
-        labels = labels.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
 
-        output=model(words)
-        loss = criterion(output,labels)
+        outputs=model(words)
+        loss = criterion(outputs,labels)
 
         optimizer.zero_grad()
         loss.backward()
@@ -85,4 +83,4 @@ for epoch in range(num_epochs):
     if( epoch +1)% 100 == 0:
         print(f'epoch {epoch+1}/{num_epochs},loss={loss.item():.4f}')
 
-print(f'final loss, loss={loss.item():.4f}')
+# print(f'final loss, loss={loss.item():.4f}')
